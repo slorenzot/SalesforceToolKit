743aaeb46a0696104b33872b875186dc20ea489a
Soulberto Lorenzo <soulberto@cloudblue.us>
2023-07-18 08:12:03 -0500

    Initial Commit

---
+++ SalesforceToolKit/SalesforceToolKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesforceToolKitApp.swift
+//  SalesforceToolKit
+//
+//  Created by Soulberto Lorenzo on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SalesforceToolKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
